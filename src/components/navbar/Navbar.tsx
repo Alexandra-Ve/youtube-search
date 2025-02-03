6201e68e75fc60d38e56b459c3be81adfac6ee7d
alexandra <fantasticbytes@gmail.com>
2025-02-04 01:20:23 +0200

    Added suspense for navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,17 @@
 import Container from "@/components/global/Container";
 import NavSearch from "./NavSearch";
+import {Suspense} from "react";
+
 
 
 function Navbar() {
     return (
         <nav className='border-b'>
             <Container className='flex flex-col items-center py-8 gap-4'>
-                <NavSearch/>
+                <Suspense>
+                    <NavSearch/>
+                </Suspense>
+
             </Container>
         </nav>
     )
